5689fb828ea2db09cc5701f5d1421e5f2dfea7d5
Jaborg <jlappinsadiq@gmail.com>
2024-04-19 14:40:40 +0100

    Implementing TDD

--- tests/test-crud.py
+++ tests/test-crud.py
@@ -2,14 +2,13 @@ import os
 
 from mock import patch
 
-from app.crud_operations import read_reviews_crud
+from app.crud_operations import pretty_reviews
 
 
-reviews = os.listdir('tests/tests_reviews')
+reviews = os.listdir('tests/test_reviews/')
 
 
 @patch('app.crud_operations.reviews',reviews)
-def test_read_reviews_curd():
-    return_value = read_reviews_crud()
-    breakpoint()
-    assert return_value == reviews
+def test_pretty_reviews():
+    return_value = pretty_reviews()
+    assert return_value == ['test review 1', 'test review 2']
