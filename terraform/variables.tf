44f0ca1aeb334476ade091569c85b83f20d25447
Jaborg <jlappinsadiq@gmail.com>
2024-05-20 18:21:04 +0100

    Fixed DNS issues

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,25 +1,25 @@
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
-  default = 8000
+  default     = 8000
 }
 
 variable "cluster_name" {
   description = "The name to use for all the cluster resources"
-  type = string
-  default = "Test_Jacob"
-  
+  type        = string
+  default     = "Test-Jacob"
+
 }
 
 variable "min_size" {
   description = "The minimum number of EC2 Instances in the ASG"
-  type = number
-  default = 1
+  type        = number
+  default     = 1
 }
 
 variable "max_size" {
   description = "The maximum number of EC2 Instances in the ASG"
-  type = number
-  default = 2
+  type        = number
+  default     = 2
 }
 
 variable "AWS_ACCESS_KEY_ID" {
